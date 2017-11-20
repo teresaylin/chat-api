de56a241403af26d295db971d3fa69b89018b869
tlin15 <tlin15@mit.edu>
2017-11-20 00:08:50 -0800

    automatically populate datetime field for messages

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -9,7 +9,7 @@ CREATE TABLE user(id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20), passw
 
 CREATE TABLE thread(id INT AUTO_INCREMENT PRIMARY KEY, user_id_1 INT, user_id_2 INT);
 
-CREATE TABLE message(id INT AUTO_INCREMENT PRIMARY KEY, time DATETIME, content TEXT, width INT, height INT, videolength INT, source TEXT, sender_id INT, thread_id INT);
+CREATE TABLE message(id INT AUTO_INCREMENT PRIMARY KEY, time DATETIME DEFAULT CURRENT_TIMESTAMP, content TEXT, width INT, height INT, videolength INT, source TEXT, sender_id INT, thread_id INT);
 
 CREATE TABLE thread_message(thread_id INT NOT NULL, message_id INT NOT NULL);
 
